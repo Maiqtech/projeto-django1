83207d8ea17a05cb9f842e9a93a2b27c09a24846
Antonio Maia <antoniiomaiia985@gmail.com>
2024-05-02 17:07:47 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
     return render(request, 'recipes/home.html', context= {
         'name': 'Antonio Maia',
     })
-
-
-def sobre(request):
-    return render (request, 'temp.html')
-
-def contato(request):
-    return HttpResponse('CONTATO')
