458d240203fe273262af2a29580fb9e7d277dbb9
Antonio Maia <antoniiomaiia985@gmail.com>
2024-05-18 23:25:31 -0300

    injetando dados na lista da view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context= {
-        'name': 'Antonio Maia',
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
-def recipe(request,id):
-    return render(request, 'recipes/pages/recipe-view.html', context= {
-        'name': 'Antonio Maia',
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
     })
