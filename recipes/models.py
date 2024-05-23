9394c766fc0f3557db231a7bcfe3772b8e2b42ba
Antonio Maia <antoniiomaiia985@gmail.com>
2024-05-22 23:53:50 -0300

    adicionando recipes na admin do django

--- recipes/models.py
+++ recipes/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 class Category (models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+       return self.name
 
 
 class Recipe (models.Model):
@@ -29,3 +31,6 @@ class Recipe (models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
